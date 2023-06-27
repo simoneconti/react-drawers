97a58deeac4c9bdd767a057cd1945f28a8cee1fe
Simone Conti <simoconti84@gmail.com>
2023-06-27 15:08:59 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export const App = () => {
   return (
     <div style={{ backgroundColor: 'white', height: '100vh', margin: 'auto', position: 'relative', width: 500 }}>
       <Drawers drawerItemStyle={{ borderBottom: 'solid 1px #ccc', padding: 5 }}>
-        <X index={0} parent="root" />
+        <X index={0} parent="" />
       </Drawers>
     </div>
   );
@@ -17,24 +17,24 @@ export const App = () => {
 
 const X = ({ index, parent }: { index: number; parent: string }) => (
   <>
-    {index < 6 ? (
+    {index < 4 ? (
       <>
         {index > 0 && (
           <DrawerBackItem>
             <DrawerItem height={30} justifyContent="start" start={{ center: 'back' }} />
           </DrawerBackItem>
         )}
-        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}-1`} />}>
-          <DrawerItem justifyContent="start" start={{ center: `${parent}-1` }} />
+        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}1-`} />}>
+          <DrawerItem justifyContent="start" start={{ center: `${parent}1` }} />
         </DrawerTriggerItem>
-        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}-2`} />}>
-          <DrawerItem justifyContent="start" start={{ center: `${parent}-2` }} />
+        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}2-`} />}>
+          <DrawerItem justifyContent="start" start={{ center: `${parent}2` }} />
         </DrawerTriggerItem>
-        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}-3`} />}>
-          <DrawerItem justifyContent="start" start={{ center: `${parent}-3` }} />
+        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}3-`} />}>
+          <DrawerItem justifyContent="start" start={{ center: `${parent}3` }} />
         </DrawerTriggerItem>
-        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}-4`} />}>
-          <DrawerItem justifyContent="start" start={{ center: `${parent}-4` }} />
+        <DrawerTriggerItem newDrawer={<X index={index + 1} parent={`${parent}4-`} />}>
+          <DrawerItem justifyContent="start" start={{ center: `${parent}4` }} />
         </DrawerTriggerItem>
       </>
     ) : (
@@ -42,7 +42,7 @@ const X = ({ index, parent }: { index: number; parent: string }) => (
         <DrawerBackItem>
           <DrawerItem height={30} justifyContent="start" start={{ center: 'back' }} />
         </DrawerBackItem>
-        <DrawerItem justifyContent="start" start={{ center: `${parent}-1` }} />
+        <DrawerItem justifyContent="start" start={{ center: `${parent}1` }} />
       </>
     )}
   </>
